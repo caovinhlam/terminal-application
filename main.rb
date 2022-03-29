c857906ee6e883c4dff0b945a03ec3b768c3879d
caovinhlam <caovinhlam@gmail.com>
2022-03-30 01:32:31 +1100

    Added account creation method

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-require "tty-prompt"
-
+require 'tty-prompt'
+require 'json'
 require_relative 'classes'
 
 prompt = TTY::Prompt.new
@@ -15,13 +15,28 @@ puts "
    |__/   \______/ |_______/  \______/ 
                                        
 "
+# Create an account
+parsed = JSON.load_file('accounts.json', symbolize_names: true)
+used_ids = parsed[0][:created_id]
+random_id = rand(100000)
+while used_ids.include?(random_id)
+    random_id(100000)
+end
 
-prompt.select("What would you like to do?", %w(Login Create_Account), cycle: true)
+parsed[0][:created_id] << random_id
 
-user = prompt.ask("Username:", required: true)
-p user
-pass = prompt.mask("Password:", required: true, echo: true)
-p pass
+new_account = { id: random_id, user: "chicken", password: "secret", tasks: [] }
+parsed << new_account
+p parsed
+File.write('accounts.json', JSON.pretty_generate(parsed))
+
+
+# prompt.select("What would you like to do?", %w(Login Create_Account), cycle: true)
+
+# user = prompt.ask("Username:", required: true)
+# p user
+# pass = prompt.mask("Password:", required: true, echo: true)
+# p pass
 task = ['hello','this','is']
 
 vinh = User.new('chicken','egg',task)
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 ```gem install bundler``` install this before installing all the dependencies
+
 ```bundle install``` to install all the dependencies for the program
 
 ## Dependencies
