5a6a7176930129fb0069e1a72d85dd9296dbed81
caovinhlam <caovinhlam@gmail.com>
2022-03-30 18:48:44 +1100

    Refactored, login, create, edit, update and delete task functionaility to use classes

--- classes.rb
+++ classes.rb
@@ -1,10 +1,44 @@
+class Tasks
+    # Reader - read only, writer - write only, accessor is both
+    attr_accessor :tasks
+
+    def initialize(task)
+        @tasks = task
+    end
+
+    def display_list()
+        @tasks.each_with_index do |task, index|
+            puts "#{index + 1}. #{task}"
+        end
+    end
+
+    def add_task(task)
+        @tasks << task
+    end
+
+    def update_task(index, new_task)
+        @tasks[index] = new_task
+    end
+
+    def delete_task(index)
+        @tasks.delete_at(index)
+    end
+end
+
 class User
     # Reader - read only, writer - write only, accessor is both
-    attr_accessor :first_name, :last_name, :task
+    attr_accessor :first_name, :last_name, :tasks
+    attr_reader :id
 
-    def initialize(first_name, last_name, task=[])
+    def initialize(id, first_name, last_name, tasks=[])
+        @id = id
         @first_name =  first_name
         @last_name = last_name
-        @task = task
+        @tasks = Tasks.new(tasks)
     end
+
+    def get_tasks()
+        return tasks.tasks
+    end
+
 end
--- main.rb
+++ main.rb
@@ -33,10 +33,23 @@ def create_account(filename, firstname, lastname, username, password)
     File.write(filename, JSON.pretty_generate(parsed))
 end
 
-# puts "JASkdljasdkljasdK".capatalize()
+def login_account(filename, username, password)
+    require_relative 'classes'
 
-def main_menu(filename, userid)
+    parsed = JSON.load_file(filename, symbolize_names: true)
+    parsed.each do |user|
+        if (user[:username] == username) && (user[:password] == password)
+            puts "You've logged in!"
+            user_account = User.new(user[:id], user[:firstname], user[:lastname], user[:tasks])
+            return user_account
+        end
+    end
+    return false
+end
+
+def main_menu(filename, user_account)
     require 'tty-prompt'
+    require_relative 'classes'
     prompt = TTY::Prompt.new
 
     parsed = JSON.load_file(filename, symbolize_names: true)
@@ -46,44 +59,81 @@ def main_menu(filename, userid)
     #     menu.choice "Create Task"
     #     menu.choice "Edit Task"
     # end
-    user_details = 0
-    parsed.each do |user|
-        if (user[:id] == userid)
-            user_details = user
-            break
-        end
-    end
 
+    # Getting user hash from the JSON file
+    # user_details = 0
+    # parsed.each do |user|
+    #     if (user[:id] == userid)
+    #         user_details = user
+    #         break
+    #     end
+    # end
+
+    user_details = user_account
+    userid = user_account.id
     p user_details
 
     menu_selection = 1
+    # While user hasn't quit the menu
     while menu_selection != 0
         choices = {"View My Task" => 1, "Create Task" => 2, "Edit Task" => 3, "Delete Task" => 4, "Quit" => 0}
         menu_selection = prompt.select("What would you like to do?", choices, cycle: true)
         case menu_selection
+        # View my task
         when 1
-            user_details[:tasks].each_with_index do |task, index|
-                puts "#{index + 1}. #{task}"
-            end
+            # List it as a numbered list
+            # user_details[:tasks].each_with_index do |task, index|
+            #     puts "#{index + 1}. #{task}"
+            # end
+            user_account.tasks.display_list
             prompt.keypress("Press any key to continue")
         when 2
+            # Appending to the task list
             task = prompt.ask('Input Task:', required: true)
-            user_details[:tasks] << task
+            # user_details[:tasks] << task
+            user_account.tasks.add_task(task)
             puts "Task Added!"
         when 3
+            # Making the task list selectable for the user to pick
             choices = {}
-            user_details[:tasks].each_with_index do |task, index|
-                # puts "#{index + 1}. #{task}"
+            # user_details[:tasks].each_with_index do |task, index|
+            task_list = user_account.get_tasks
+            task_list.each_with_index do |task, index|
+                # key is the task and the value is index of the task in the array
                 choices["#{index+1}. #{task}"] = index
             end
-            task_selection = prompt.select("Which task would you like to edit?", choices, cyle: true)
-            task = prompt.ask("What is the new task?")
-            user_details[:tasks][task_selection] = task
+            # Get index position from the hash to edit the task in the array in user details
+            task_index = prompt.select("Which task would you like to edit?", choices, cyle: true)
+            new_task = prompt.ask("What is the new task?")
+            # Replace old task with new editted task
+            # user_details[:tasks][task_selection] = task
+            user_account.tasks.update_task(task_index, new_task)
             puts "Task Updated!"
         when 4
-            puts 4
+            # Making the task list selectable for the user to pick
+            choices = {}
+            task_list = user_account.get_tasks
+            task_list.each_with_index do |task, index|
+            # user_details[:tasks].each_with_index do |task, index|
+                choices["#{index+1}. #{task}"] = index
+            end
+            # Get index position from the hash to delete task in the array in user details
+            task_index = prompt.select("Which task would you like to delete?", choices, cyle: true)
+            confirm = prompt.yes?("Are you sure?")
+            if confirm
+                # user_details[:tasks].delete_at(task_selection)
+                user_account.tasks.delete_task(task_index)
+                puts "Task Deleted!"
+            end
         when 0
             menu_selection = 0
+            parsed.each do |user|
+                if (user[:id] == user_account.id)
+                    user[:tasks] = user_account.get_tasks
+                end
+            end
+            File.write(filename, JSON.pretty_generate(parsed))
+            puts "DataBase UPDATED"
             puts "byebye"
         end
     end
@@ -98,18 +148,17 @@ case menu_selection
 when 'login'
     puts "You have selected Login"
 
-    username = prompt.ask('Username:', required: true).downcase
-    
-    password = prompt.mask("Password:", required: true, echo: true)
-    parsed = JSON.load_file(filename, symbolize_names: true)
-
-    parsed.each do |user|
-        if (user[:username] == username) && (user[:password] == password)
-            puts "You've logged in!"
-            user = user[:id]
-            main_menu(filename, user)
+    login = false
+    while !login 
+        username = prompt.ask('Username:', required: true).downcase
+        password = prompt.mask("Password:", required: true, echo: true)
+        user_account = login_account(filename, username, password)
+        if user_account
+            login = true
+            main_menu(filename, user_account)
         end
     end
+
 when 'create account'
     puts "You have selected Create account"
 
